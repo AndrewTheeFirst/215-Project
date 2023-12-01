f4ae47f3fc86f92a7d52093b39657067919654b1
MSI\chris <christopherpillgreen@gmail.com>
2023-12-01 10:29:25 -0500

    GUI window with enter button and popup window

---
+++ Project Resouces/Project.py
@@ -0,0 +1,44 @@
+#Andrew Patton
+#Christopher Pillgreen
+
+from PySide6.QtWidgets import \
+     QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QAbstractItemView, QMessageBox, QWidget, QListWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        
+        self.setWindowTitle("Book Finder")
+        self.resize(300,40)
+
+        layout = QVBoxLayout()
+
+        self.input = QLineEdit()
+        self.input.setPlaceholderText("Please enter an ISBN Number")
+        layout.addWidget(self.input)
+
+        addButton = QPushButton("Enter")
+        addButton.clicked.connect(self.on_enter_clicked)  
+        layout.addWidget(addButton)
+        
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+        self.input.returnPressed.connect(self.on_enter_clicked)
+
+    def on_enter_clicked(self):
+        entered_text = self.input.text()
+        QMessageBox.information(self, f"Book Title Here{entered_text}", "Book Prices Here")
+        
+    
+   
+
+
+       
+if __name__=="__main__":
+    app = QApplication()
+    window = MainWindow()
+    window.show()
+    app.exec()
